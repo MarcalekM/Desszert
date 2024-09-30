6ca9205662798f77d0da3d67abd9281618807cdb
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-09-30 11:21:37 +0200

    Add project files.

---
+++ Desszert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desszert
+{
+    internal class Dessert
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool Awarded { get; set; }
+        public int Price { get; set; }
+        public string Amount { get; set; }
+
+        public Dessert(string sor) {
+            var i = sor.Split(';');
+            Name = i[0];
+            Type = i[1];
+            Awarded = bool.Parse(i[2]);
+            Price = int.Parse(i[3]);
+            Amount = i[4];
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Desszert;
+
+namespace Desszert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Dessert> desszertek = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\palma.txt",
+                encoding: UTF32Encoding.UTF8);
+            while (!sr.EndOfStream) desszertek.Add(new(sr.ReadLine()));
+            Ajanlat.Text = "Mai ajánlatunk: " + desszertek[25].Name;
+            Dijazott.Text = desszertek.Where(d => d.Awarded == true).Count().ToString() + " féle díjnyertes édességből választhat!";
+            LegdragabbNev.Text = desszertek.MaxBy(d => d.Price).Name.ToString();
+            LegolcsobbNev.Text = desszertek.MinBy(d => d.Price).Name.ToString();
+        }
+    }
+}
